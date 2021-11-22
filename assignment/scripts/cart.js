f389071f0b15997afcb6d1dc6c741004a1e53f43
Chris Agbolade <yemluck@gmail.com>
2021-11-22 02:09:40 -0600

    checkpoint 2

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -5,14 +5,7 @@ console.log('***** Cart Functions *****');
 let basket = [];
 const maxItems = 5;
 
-function isFull() {
-    let basketSize = true;
-    if ( basket.length < maxItems){ 
-        basketSize = false;
-    }
-    return basketSize;
-}
-
+//function to add items to the basket
 function addItem(item){
     item = String(item);
     let output = true;
@@ -25,21 +18,42 @@ function addItem(item){
     return output;
 }
 
-console.log('Expect to see "true";', addItem('novel'));
-addItem('textbook');
-addItem(4);
-addItem(false); // every item was added. Couldn't figure out how to reject
-                // non string items
-
-function listItems(){
-     for(x of basket){
-        console.log(x);
-    }
-    return basket;
- }
-
- function empty(){
+//function to empty basket
+function empty(){
     basket = [];  // reset the 'basket' to an empty array
     return basket;
  }
-console.log(basket);
+console.log('Check to see if the basket is empty', basket);
+
+
+// function to check if basket is full or not
+function isFull() {
+    let basketSize = true;
+    if ( basket.length < maxItems){ 
+        basketSize = false;
+    }
+    return basketSize;
+}
+
+// function to list items in basket
+function listItems(){
+    for(x of basket){
+       console.log(x);
+   }
+   return basket;
+}
+
+// Checks
+console.log('Expect to see "true";', addItem('novel'));
+console.log('Expect to see "true";', addItem('textbook'));
+console.log('Expect to see "true";', addItem(4));
+console.log('Expect to see "true";', addItem(true)); // couldn't figure out how to reject non-string parameters
+
+
+/* Create a function called `removeItem`. It should:
+  - Take an input parameter for a string `item`
+  - Use [Array.indexOf](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf) to find the index of the first matching item in the basket.
+  - Use [Array.splice](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice) to remove the first matching item from the basket.
+  - Return the item removed or `null` if the item was not found 
+  */
+
